e508e83a7edd1dd9d4ea9da77e70235bbfa448ad
Dmitry Martsul <be9po@outlook.com>
2019-03-17 18:54:13 +0300

    Add API TelegramBot

---
+++ src/main/java/KabanBot.java
@@ -0,0 +1,30 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class KabanBot extends TelegramLongPollingBot {
+
+
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "Kaban_666_bot";
+    }
+
+    public String getBotToken() {
+        return "634261680:AAFr4uAvT6J93DTOg_bm5WxGO5y6bp7-ngk";
+    }
+}
